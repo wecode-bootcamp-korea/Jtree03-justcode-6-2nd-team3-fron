d5da0fa968fe936bb0df7d2575200315bdb64013
sh.inv <qierapu1im@gmail.com>
2022-09-19 11:38:35 +0900

    초기셋팅

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Router from "./pages/Router";
+import Router from "./Router";
 
 import "./styles/common.scss";
 import "./styles/reset.scss";
