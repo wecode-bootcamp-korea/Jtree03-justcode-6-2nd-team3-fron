88ba3d3e8dd21f845c5a75129242f9e57a482780
GPontheG <gpontheg@gmail.com>
2022-09-23 18:05:26 +0900

    Add: 기업회원 조건부 렌더링

--- src/components/Login/Signup/SignupForm.js
+++ src/components/Login/Signup/SignupForm.js
@@ -1,19 +1,23 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-// import { useHistory } from 'react-router-dom'
-// import axios from 'axios'
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 import styled from 'styled-components';
 import Form from '../../LayOut/Form';
 import FormTop from '../../LayOut/FormTop';
 
 export default function SignupForm() {
-  // const navigate = useNavigate();
-  // const {replace} = useHistory();
+  const navigate = useNavigate();
   const [userName, setUserName] = useState('');
   const [password, setPassword] = useState('');
   const [email, setEmail] = useState('');
   const [name, setName] = useState('');
   const [nickname, setNickname] = useState('');
+  const [userType, setUserType] = useState('1');
+  const [BusinessName, setBusinessName] = useState('');
+  const [BusinessEmail, setBusinessEmail] = useState('');
+  const [BusinessNum, setBusinessNum] = useState('');
+  const [BusinessPhone, setBusinessPhone] = useState('');
+  const [BusinessIntro, setBusinessIntro] = useState('');
 
   const onUserNameHandler = event => {
     setUserName(event.currentTarget.value);
@@ -35,33 +39,95 @@ export default function SignupForm() {
     setNickname(event.currentTarget.value);
   };
 
-  // useEffect(()=>{
-  // const onSubmit = ()=>{
-  // axios
-  //   .post('/signup'), {
-  //     id: userName,
-  //     password: password,
-  //     email: email,
-  //     user_name: name,
-  //     nickname: nickname,
-  //     // user_type: userType,
-  //   }
-  //   .then((response) => {
-  //     console.log('회원가입 성공!');
-  //     console.log('user_profile', response.data.user);
-  //     console.log('user_token', response.data.jwt);
-  //     localStorage.setItem('token', response.data.jwt);
-  //     replace('/');
-  //   })
+  const onUserTypeHandler = event => {
+    setUserType(event.currentTarget.id);
+  };
+
+  const onBusinessNameHandler = event => {
+    setBusinessName(event.currentTarget.value);
+  };
+
+  const onBusinessNumHandler = event => {
+    setBusinessNum(event.currentTarget.value);
+  };
+
+  const onBusinessEmailHandler = event => {
+    setBusinessEmail(event.currentTarget.value);
+  };
+
+  const onBusinessPhoneHandler = event => {
+    setBusinessPhone(event.currentTarget.value);
+  };
+
+  const onBusinessIntroHandler = event => {
+    setBusinessIntro(event.currentTarget.value);
+  };
+
+  const onSubmit = () => {
+    axios
+      .post('/signup', {
+        id: userName,
+        password: password,
+        email: email,
+        user_name: name,
+        nickname: nickname,
+        user_type: userType,
+      })
+
+      .then(response => {
+        console.log('회원가입 성공!');
+        // console.log('user_profile', response.data.user);
+        // console.log('user_token', response.data.jwt);
+        // localStorage.setItem('token', response.data.jwt);
+        navigate('/');
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  const onSubmitBusiness = () => {
+    // const url = `/api/signup`;
+    // const formData = new FormData();
+    // formData.append('id', userName);
+    // formData.append('password', password);
+    // formData.append('user_name', name);
+    // formData.append('nickname', nickname);
+    // formData.append('user_type', userType);
+    // formData.append('Business_name', businessName);
+    // formData.append('introduction', businessIntro);
+    // formData.append('business_registration_number', businessNum);
+    // formData.append('contact_information', businessContact);
+    // formData.append('Business_email', businessEmail);
+    // formData.append('filename', businessRegistration);
+    // const config = {
+    //   headers: {
+    //     'content-type': 'multipart/form-data',
+    //   },
+    // };
+    // return axios.post(url, formData, config);
+  };
 
   return (
     <Form>
       <FormTop />
       <FirstLine>회원가입에 필요한 기본정보를 입력해주세요.</FirstLine>
       <RadioBtns>
-        <RadioBtn type="radio" />
+        <RadioBtn
+          type="radio"
+          name="userTypeBtn"
+          onClick={onUserTypeHandler}
+          value={userType}
+          id="1"
+        />
         <RadioLabel>개인회원</RadioLabel>
-        <RadioBtn type="radio" />
+        <RadioBtn
+          type="radio"
+          name="userTypeBtn"
+          onClick={onUserTypeHandler}
+          value={userType}
+          id="2"
+        />
         <RadioLabel>기업회원</RadioLabel>
       </RadioBtns>
       <InputWrapper>
@@ -109,21 +175,71 @@ export default function SignupForm() {
           placeholder="알파벳, 한글, 숫자를 20자 이하로 입력해주세요"
         />
       </InputWrapper>
-      {/* <div
-        className="g-recaptcha"
-        data-sitekey="6LeedxgiAAAAAK4Qt-X-eL1uJre415TP0b-YEsSv"
-      ></div>
-      <script
-        src="https://www.google.com/recaptcha/api.js"
-        async
-        defer
-      ></script> */}
-      <Button>회원가입</Button>
+      {userType === '2' && (
+        <SecondLine>기업회원가입에 필요한 추가정보를 입력해주세요.</SecondLine>
+      )}
+      {userType === '2' && (
+        <InputWrapper>
+          <InputLabel>회사명</InputLabel>
+          <Input
+            type="text"
+            value={BusinessName}
+            onChange={onBusinessNameHandler}
+            placeholder="회사명을 입력해주세요"
+          />
+        </InputWrapper>
+      )}
+      {userType === '2' && (
+        <InputWrapper>
+          <InputLabel>사업자 등록번호</InputLabel>
+          <Input
+            type="text"
+            value={BusinessNum}
+            onChange={onBusinessNumHandler}
+            placeholder="사업자 등록번호를 입력해주세요"
+          />
+        </InputWrapper>
+      )}
+      {userType === '2' && (
+        <InputWrapper>
+          <InputLabel>대표연락처</InputLabel>
+          <Input
+            type="text"
+            value={BusinessPhone}
+            onChange={onBusinessPhoneHandler}
+            placeholder="연락처를 입력해주세요"
+          />
+        </InputWrapper>
+      )}
+      {userType === '2' && (
+        <InputWrapper>
+          <InputLabel>대표이메일</InputLabel>
+          <Input
+            type="text"
+            value={BusinessEmail}
+            onChange={onBusinessEmailHandler}
+            placeholder="kimcode@justcode.com"
+          />
+        </InputWrapper>
+      )}
+      {userType === '2' && (
+        <RegistrationWrapper>
+          <InputLabel>회사소개</InputLabel>
+          <Text
+            value={BusinessIntro}
+            onChange={onBusinessIntroHandler}
+            placeholder="사업자등록증을 추가하시고 회사를 소개해주세요."
+          />
+          <input type="file" />
+        </RegistrationWrapper>
+      )}
+      {userType === '1' && <Button onClick={onSubmit}>회원가입</Button>}
+      {userType === '2' && <Button onClick={onSubmitBusiness}>회원가입</Button>}
       <QuestionWrapper>
         <Question>
           이미 회원이신가요?
           <Link to="/login">
-            <ToLogin> 로그인</ToLogin>
+            <span> 로그인</span>
           </Link>
         </Question>
       </QuestionWrapper>
@@ -137,6 +253,33 @@ const FirstLine = styled.div`
   align-items: center;
   color: #6b7280;
   margin-top: 20px;
+  font-size: 14px;
+  :before {
+    content: '';
+    flex-grow: 1;
+    margin-right: 5px;
+    background-color: #6b7280;
+    height: 1px;
+    font-size: 0px;
+    line-height: 0px;
+  }
+  :after {
+    content: '';
+    flex-grow: 1;
+    margin-left: 5px;
+    background-color: #6b7280;
+    height: 1px;
+    font-size: 0px;
+    line-height: 0px;
+  }
+`;
+
+const SecondLine = styled.div`
+  display: flex;
+  flex-basis: 100%;
+  align-items: center;
+  color: #6b7280;
+  margin-top: 35px;
   font-size: 14px;
   :before {
     content: '';
@@ -177,6 +320,14 @@ const InputWrapper = styled.div`
   width: 448px;
   height: 66px;
 `;
+
+const RegistrationWrapper = styled.div`
+  margin-top: 30px;
+  margin-bottom: 70px;
+  width: 448px;
+  height: 66px;
+`;
+
 const InputLabel = styled.label`
   width: 448px;
   height: 20px;
@@ -193,6 +344,17 @@ const Input = styled.input`
   border-radius: 5px;
   border: 0.5px solid #6b7280;
 `;
+
+const Text = styled.textarea`
+  width: 448px;
+  height: 100px;
+  font-size: 16px;
+  padding: 8px 12px;
+  margin-top: 7px;
+  border-radius: 5px;
+  border: 0.5px solid #6b7280;
+`;
+
 const Button = styled.button`
   width: 448px;
   height: 38px;
@@ -219,4 +381,3 @@ const Question = styled.span`
   font-size: 14px;
   color: #6b7280;
 `;
-const ToLogin = styled.span``;
