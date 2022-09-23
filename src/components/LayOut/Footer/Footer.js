eac7388df9c93ca6e2b894e8c913542c74dac8cd
hrim <>
2022-09-23 21:53:22 +0900

    Modify: css 수정

--- src/components/LayOut/Footer/Footer.js
+++ src/components/LayOut/Footer/Footer.js
@@ -1,28 +1,41 @@
-import React from 'react'
-import styled from 'styled-components'
-import FooterLeft from './FooterLeft'
-import FooterCenter from './FooterCenter'
-import FooterRight from './FooterRight'
+import React from 'react';
+import styled from 'styled-components';
+import FooterLeft from './FooterLeft';
+import FooterCenter from './FooterCenter';
+import FooterRight from './FooterRight';
 
 export default function Footer() {
-
   return (
-    <Wrapper>
-      <Style>
-        <FooterLeft />
-        <FooterCenter />
-        <FooterRight />
-      </Style>
-    </Wrapper>
-  )
+    <Container>
+      {' '}
+      <Wrapper>
+        <Style>
+          <FooterLeft />
+          <FooterCenter />
+          <FooterRight />
+        </Style>
+      </Wrapper>
+    </Container>
+  );
 }
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  padding: 15px 0;
+  border-top: 0.5px solid black;
+`;
 
 const Wrapper = styled.div`
   padding: 28px 0px;
-  border-top: 0.5px solid black;
-`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 1280px;
+`;
+
 const Style = styled.div`
   width: auto;
   height: 104px;
   display: flex;
-`
+`;
