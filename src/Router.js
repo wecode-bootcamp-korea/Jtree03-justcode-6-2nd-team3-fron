dc7570755ee55582a5f122a177e04bf37e54f81f
hrimwk <104122566+hrimwk@users.noreply.github.com>
2022-09-23 18:24:37 +0900

    Feature/new post (#4)
    * Add: 게시판 스타일링 UI 추가
    
    * Add: 게시글 작성 스타일링 툴박스 옵션값
    
    * Add: select라이브러리 추가
    
    * Modify: 태그 검색 컴포넌트 분리
    
    * Add: reCaptcha추가
    Modify: options 위치 변경 , 컴포넌트 폴더 위치 수정
    
    * Modify: selectTag 수정
    
    * Modify: 새글작성 컴포넌트 분리
    Add: 새글작성 라우터 추가
    
    Co-authored-by: hrim <>

--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,10 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 // 메인페이지
-import Main from "./pages/Main";
+import Main from './pages/Main';
+import QuestionsNew from './pages/NewPost/QuestionsNew';
+import EventsNew from './pages/NewPost/EventsNew';
+import KnowledgeNew from './pages/NewPost/KnowledgeNew';
 
 // 글 목록
 import { QnA } from './pages/List';
@@ -9,11 +12,11 @@ import { QnA } from './pages/List';
 import Detail from './pages/DetailPage/Detail';
 
 // 내 프로필
-import Settings from "./pages/Settings";
+import Settings from './pages/Settings';
 // 비밀번호 변경
-import ChangePasswordConfirm from "./pages/Settings/ProfileSetting/ChangePassWord/ChangePasswordConfirm";
+import ChangePasswordConfirm from './pages/Settings/ProfileSetting/ChangePassWord/ChangePasswordConfirm';
 // 회원 탈퇴
-import SecessionTab from "./pages/Settings/ProfileSetting/Secession/SecessionTab";
+import SecessionTab from './pages/Settings/ProfileSetting/Secession/SecessionTab';
 
 function Router() {
   return (
@@ -21,6 +24,9 @@ function Router() {
       <Routes>
         {/* 메인페이지 */}
         <Route path="/" element={<Main />} />
+        <Route path="/questions/new" element={<QuestionsNew />} />
+        <Route path="events/new" element={<EventsNew />} />
+        <Route path="knowledge/new" element={<KnowledgeNew />} />
         {/* 상세페이지 */}
         <Route path="/articles/1" element={<Detail />} />
 
@@ -30,7 +36,10 @@ function Router() {
         {/* 내 프로필 */}
         <Route path="/settings" element={<Settings />} />
         {/* 비밀번호 변경 */}
-        <Route path="/settings/password-changes" element={<ChangePasswordConfirm />} />
+        <Route
+          path="/settings/password-changes"
+          element={<ChangePasswordConfirm />}
+        />
         {/* 회원 탈퇴 */}
         <Route path="/user/withdrawConfirm" element={<SecessionTab />} />
       </Routes>
