3b330c0eb485db87c945e251f8724a7b5b0e8fe3
hrim <>
2022-09-25 00:59:15 +0900

    Revert "Modify: header footer"
    This reverts commit a68dcba757bb963407ada3d8781f83cf85ef7e43.

--- src/components/LayOut/Footer/Footer.js
+++ src/components/LayOut/Footer/Footer.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import styled from 'styled-components';
-import FooterLeft from './FooterLeft';
-import FooterCenter from './FooterCenter';
-import FooterRight from './FooterRight';
+import React from 'react'
+import styled from 'styled-components'
+import FooterLeft from './FooterLeft'
+import FooterCenter from './FooterCenter'
+import FooterRight from './FooterRight'
 
 export default function Footer() {
+
   return (
     <Wrapper>
       <Style>
@@ -13,15 +14,15 @@ export default function Footer() {
         <FooterRight />
       </Style>
     </Wrapper>
-  );
+  )
 }
 
 const Wrapper = styled.div`
   padding: 28px 0px;
   border-top: 0.5px solid black;
-`;
+`
 const Style = styled.div`
   width: auto;
   height: 104px;
   display: flex;
-`;
+`
--- src/components/LayOut/Header/Header.js
+++ src/components/LayOut/Header/Header.js
@@ -1,30 +1,32 @@
-import React from 'react';
-import styled from 'styled-components';
-import HeaderLogo from './HeaderLogo';
-import HeaderNav from './HeaderNav';
-import HeaderBtns from './HeaderBtns';
+import React from 'react'
+import styled from 'styled-components'
+import HeaderLogo from './HeaderLogo'
+import HeaderNav from './HeaderNav'
+import HeaderBtns from './HeaderBtns'
 
 export default function Header() {
+
   return (
-    <Container>
-      <Wrapper>
-        <HeaderLogo />
-        <HeaderNav />
-        <HeaderBtns />
-      </Wrapper>
-    </Container>
-  );
+    <Wrapper>
+        <Style>
+            <HeaderLogo />
+            <HeaderNav />
+            <HeaderBtns />
+        </Style>
+    </Wrapper>
+  )
 }
-const Container = styled.div`
-  width: 100%;
+
+const Wrapper = styled.div `
+  padding: 20px 0px;
+  border-bottom: 0.5px solid black;
+`
+const Style = styled.div `
+  width: auto;
+  height: 35px;
   display: flex;
-  justify-content: center;
-  border-bottom: 0.5px solid #ddd;
-`;
-const Wrapper = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  width: 1280px;
-  padding: 15px 0;
-`;
+`
+
+
+
+
