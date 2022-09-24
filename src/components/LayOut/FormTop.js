8ac71cf069799baca9ce448ff11980a0fc208eb2
GPontheG <gpontheg@gmail.com>
2022-09-24 20:29:25 +0900

    Modify: Login UI

--- src/components/LayOut/FormTop.js
+++ src/components/LayOut/FormTop.js
@@ -1,67 +1,79 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
 
 export default function FormTop() {
   return (
     <Wrapper>
-        <Logo>OKIDOKI</Logo>
-        <Welcome>오키도키에 오신 것을 환영합니다.</Welcome>
-        <WebsiteDesc>OKIDOKI는 소프트웨어 개발자를 위한 지식공유 플랫폼입니다.</WebsiteDesc>
-        <SocialMediaTitle>SNS 회원가입</SocialMediaTitle>
-        <BtnWrapper>
-          <SocialMediaBtn><Icon img src='image/github.png' alt='깃헙' /></SocialMediaBtn>
-          <SocialMediaBtn><Icon img src='image/facebook.png' alt='페이스북'/></SocialMediaBtn>
-          <SocialMediaBtn><Icon img src='image/google.png' alt='구글' /></SocialMediaBtn>
-        </BtnWrapper>
-        <SecondBtnWrapper>
-          <SocialMediaBtn><Icon img src='image/N.png' alt='네이버' /></SocialMediaBtn>
-          <SocialMediaBtn><Icon imr src='image/kakao-talk.png' alt='카카오톡' /></SocialMediaBtn>
-        </SecondBtnWrapper>
+      <Logo>OKIDOKI</Logo>
+      <Welcome>오키도키에 오신 것을 환영합니다.</Welcome>
+      <WebsiteDesc>
+        OKIDOKI는 소프트웨어 개발자를 위한 지식공유 플랫폼입니다.
+      </WebsiteDesc>
+      <SocialMediaTitle>SNS 회원가입</SocialMediaTitle>
+      <BtnWrapper>
+        <SocialMediaBtn>
+          <Icon img src="image/github.png" alt="깃헙" />
+        </SocialMediaBtn>
+        <SocialMediaBtn>
+          <Icon img src="image/facebook.png" alt="페이스북" />
+        </SocialMediaBtn>
+        <SocialMediaBtn>
+          <Icon img src="image/google.png" alt="구글" />
+        </SocialMediaBtn>
+      </BtnWrapper>
+      <SecondBtnWrapper>
+        <SocialMediaBtn>
+          <Icon img src="image/N.png" alt="네이버" />
+        </SocialMediaBtn>
+        <SocialMediaBtn>
+          <Icon imr src="image/kakao-talk.png" alt="카카오톡" />
+        </SocialMediaBtn>
+      </SecondBtnWrapper>
     </Wrapper>
-  )
+  );
 }
 
-const Wrapper = styled.div `
+const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-`
-const Logo = styled.div `
-    font-size: 30px;
-`
-const Welcome = styled.span `
-    font-size: 25px;
-    font-weight: bold;
-    margin-top: 24px;
-`
-const WebsiteDesc = styled.span `
-    font-size: 13px;
-    font-weight: lighter;
-    color: #4b5563;
-    margin-top: 15px;
-`
-const SocialMediaTitle = styled.span `
+`;
+const Logo = styled.div`
+  font-size: 30px;
+`;
+const Welcome = styled.span`
+  font-size: 25px;
+  font-weight: bold;
+  margin-top: 24px;
+`;
+const WebsiteDesc = styled.span`
+  font-size: 13px;
+  font-weight: lighter;
+  color: #4b5563;
+  margin-top: 15px;
+`;
+const SocialMediaTitle = styled.span`
   margin-top: 30px;
   margin-right: 350px;
   font-weight: lighter;
   color: #4b5563;
-`
-const BtnWrapper = styled.div `
+`;
+const BtnWrapper = styled.div`
   width: 448px;
   height: 40px;
   margin-top: 10px;
   display: flex;
   justify-content: space-between;
-`
-const SecondBtnWrapper = styled.div `
+`;
+const SecondBtnWrapper = styled.div`
   width: 448px;
   height: 40px;
   margin-top: 10px;
   display: flex;
-`
+`;
 
-const SocialMediaBtn = styled.button `
+const SocialMediaBtn = styled.button`
   width: 142px;
   height: 38px;
   padding: 8px 16px;
@@ -73,8 +85,8 @@ const SocialMediaBtn = styled.button `
   align-items: center;
   border: 0.5px solid gray;
   cursor: pointer;
-`
-const Icon = styled.img `
+`;
+const Icon = styled.img`
   width: 20px;
   height: 20px;
-`
+`;
--- src/components/Login/LoginForm.js
+++ src/components/Login/LoginForm.js
@@ -1,51 +1,61 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import styled from 'styled-components'
-import Form from '../LayOut/Form'
-import FormTop from '../LayOut/FormTop'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import Form from '../LayOut/Form';
+import FormTop from '../LayOut/FormTop';
 
 export default function LoginForm() {
   return (
     <Form>
-    <FormTop />
-    <InputWrapper>
-      <InputLabel>아이디</InputLabel>
-      <Input type="text" />
-    </InputWrapper>
-    <InputWrapper>
-      <InputLabel>비밀번호</InputLabel>
-      <Input type="text" />
-    </InputWrapper>
-    <Button>로그인</Button>
-    <QuestionWrapper>
-      <Question>아직 회원이 아니신가요?
-        <Link to ='/signup'>
-          <ToSignup>  회원가입</ToSignup>
-        </Link>
-      </Question>
-    </QuestionWrapper>
-  </Form>
-  )
+      <FormTop />
+      <IdInputWrapper>
+        <InputLabel>아이디</InputLabel>
+        <Input type="text" />
+      </IdInputWrapper>
+      <PwdInputWrapper>
+        <InputLabel>비밀번호</InputLabel>
+        <Input type="password" />
+      </PwdInputWrapper>
+      <Button>로그인</Button>
+      <QuestionWrapper>
+        <Question>
+          아직 회원이 아니신가요?
+          <Link to="/signup">
+            <span> 회원가입</span>
+          </Link>
+        </Question>
+      </QuestionWrapper>
+    </Form>
+  );
 }
 
-const InputWrapper = styled.div `
+const IdInputWrapper = styled.div`
+  margin-top: 20px;
   width: 448px;
   height: 66px;
-`
-const InputLabel = styled.label `
+`;
+const PwdInputWrapper = styled.div`
+  margin-top: 10px;
+  width: 448px;
+  height: 66px;
+`;
+const InputLabel = styled.label`
   width: 448px;
   height: 20px;
+  color: #6b7280;
+  font-size: 15px;
   font-weight: lighter;
-`
-const Input = styled.input `
+`;
+const Input = styled.input`
   width: 448px;
   height: 42px;
+  font-size: 16px;
   padding: 8px 12px;
-  margin-top: 4px;
+  margin-top: 7px;
   border-radius: 5px;
-  border: 0.5px solid black;
-`
-const Button = styled.button `
+  border: 0.5px solid #6b7280;
+`;
+const Button = styled.button`
   width: 448px;
   height: 38px;
   color: #ffffff;
@@ -53,16 +63,20 @@ const Button = styled.button `
   padding: 8px 16px;
   border-radius: 10px;
   border-style: none;
-  margin-top: 10px;;
-`
-const QuestionWrapper = styled.div `
-  margin-top: 10px;
+  margin-top: 23px;
+  font-size: 14px;
+  cursor: pointer;
+  :hover {
+    background-color: #0f7ed3;
+  }
+`;
+const QuestionWrapper = styled.div`
+  margin-top: 30px;
   display: flex;
   justify-content: center;
-`
-
-const Question = styled.span `
+`;
+const Question = styled.span`
   font-weight: lighter;
-`
-const ToSignup = styled.span `
-`
+  font-size: 14px;
+  color: #6b7280;
+`;
