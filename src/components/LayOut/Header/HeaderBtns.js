500cc51ec8ad9965314af2c74997cf6222f960c5
hrimwk <104122566+hrimwk@users.noreply.github.com>
2022-09-26 18:13:33 +0900

    헤더 푸터 레이아웃 수정 (#11)
    * Modify: css 수정
    
    * Modify: Header, footer 레이아웃 수정
    
    * Delete: 불필요한 요소 주석 삭제
    
    Co-authored-by: hrim <>

--- src/components/LayOut/Header/HeaderBtns.js
+++ src/components/LayOut/Header/HeaderBtns.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import styled from 'styled-components'
-import LoginBtn from './LoginBtn'
-import SignupBtn from './SignupBtn'
+import React from 'react';
+import styled from 'styled-components';
+import LoginBtn from './LoginBtn';
+import SignupBtn from './SignupBtn';
 
 export default function HeaderBtns() {
   return (
@@ -9,13 +9,12 @@ export default function HeaderBtns() {
       <LoginBtn />
       <SignupBtn />
     </Wrapper>
-  )
+  );
 }
 
-const Wrapper = styled.div `
+const Wrapper = styled.div`
   display: flex;
-  justify-content: space-around;
+  justify-content: space-between;
   align-items: center;
-  margin-left: 450px;
-  width: 200px;
-`
+  width: 180px;
+`;
