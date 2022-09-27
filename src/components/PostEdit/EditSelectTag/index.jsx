4fc16fdec0308968f3bc85f8229319dffe20fd6b
hrim <>
2022-09-27 20:05:21 +0900

    Add: 게시글 수정 컴포넌트

---
+++ src/components/PostEdit/EditSelectTag/index.jsx
@@ -0,0 +1,66 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import CreatableSelect from 'react-select/creatable';
+
+function EditSelectTag(props) {
+  const { placeholder, getSelectValue } = props;
+  const [tagValue, setTag] = useState([]);
+  const [inputValue, setInput] = useState('');
+  const [options, setOptions] = useState([]);
+
+  // const options = [
+  //   { id: 1, value: '1', label: '1' },
+  //   { id: 2, value: '2', label: '2' },
+  //   { id: 3, value: '3', label: '3' },
+  // ];
+
+  useEffect(() => {
+    axios.get(`http://localhost:8000/tags?keyword=${inputValue}`).then(res => {
+      const newOption = res.data.tagList;
+
+      setOptions(
+        newOption.map(data => {
+          return {
+            id: data.tag_id,
+            value: data.tag_name,
+            label: data.tag_name,
+          };
+        })
+      );
+    });
+  }, [inputValue]);
+
+  const handleChange = value => {
+    setTag(
+      value.map(data => {
+        if (data.id === undefined) {
+          return { tag_name: data.value };
+        } else {
+          return { tag_id: data.id, tag_name: data.value };
+        }
+      })
+    );
+  };
+  console.log('알고싶다', tagValue);
+  useEffect(() => {
+    getSelectValue(tagValue);
+  }, [tagValue]);
+  const onInputChange = value => {
+    setInput(value);
+  };
+
+  return (
+    <>
+      <CreatableSelect
+        isMulti
+        onChange={handleChange}
+        valueKey="value"
+        options={options}
+        placeholder={placeholder}
+        onInputChange={onInputChange}
+      />
+    </>
+  );
+}
+
+export default EditSelectTag;
